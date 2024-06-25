bc9781f7c67544990d58899d18ccb0bb9f011ab3
alexsimon7 <alexanderleesimon@gmail.com>
2024-06-25 12:02:42 -0400

    added additional tokens and comments for some methods and functions

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -6,9 +6,9 @@ import (
 
 type Lexer struct {
 	input        string
-	position     int
-	readPosition int
-	ch           byte
+	position     int  // current position - points to the current char
+	readPosition int  // current reading position - points to the next char
+	ch           byte // current char being read
 }
 
 func New(input string) *Lexer {
@@ -17,6 +17,9 @@ func New(input string) *Lexer {
 	return l
 }
 
+// The readChar method sets the ch field to the next character in the input string
+// (pointed at by readPosition), sets the current position to the next position (readPosition)
+// and increments readPosition by one to the next character in the input string
 func (l *Lexer) readChar() {
 	if l.readPosition >= len(l.input) {
 		l.ch = 0
@@ -28,6 +31,8 @@ func (l *Lexer) readChar() {
 	l.readPosition += 1
 }
 
+// The readPosition method reads an identifier advancing the position field until a non-letter
+// character is found and then returns the identifer
 func (l *Lexer) readIdentifier() string {
 	position := l.position
 
@@ -38,6 +43,8 @@ func (l *Lexer) readIdentifier() string {
 	return l.input[position:l.position]
 }
 
+// The readNumber method reads a Number advancing the position field until a non-number or
+// decimal character is found and then returns the number or float
 func (l *Lexer) readNumber() (string, token.TokenType) {
 	tokenToUse := token.INT
 	position := l.position
@@ -57,9 +64,19 @@ func (l *Lexer) readNumber() (string, token.TokenType) {
 
 }
 
+// The skipWhitespace method advances the field position past any spaces, tabs, newline,
+// or carriage returns in the input string
 func (l *Lexer) skipWhitespace() {
 	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
 		l.readChar()
+	}
+}
+
+func (l *Lexer) peekChar() byte {
+	if l.readPosition >= len(l.input) {
+		return 0
+	} else {
+		return l.input[l.readPosition]
 	}
 }
 
@@ -70,7 +87,14 @@ func (l *Lexer) NextToken() token.Token {
 
 	switch l.ch {
 	case '=':
-		tok = newToken(token.ASSIGN, l.ch)
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.EQ, Literal: literal}
+		} else {
+			tok = newToken(token.ASSIGN, l.ch)
+		}
 	case ';':
 		tok = newToken(token.SEMICOLON, l.ch)
 	case '(':
@@ -85,6 +109,25 @@ func (l *Lexer) NextToken() token.Token {
 		tok = newToken(token.LBRACE, l.ch)
 	case '}':
 		tok = newToken(token.RBRACE, l.ch)
+	case '-':
+		tok = newToken(token.MINUS, l.ch)
+	case '!':
+		if l.peekChar() == '=' {
+			ch := l.ch
+			l.readChar()
+			literal := string(ch) + string(l.ch)
+			tok = token.Token{Type: token.NOT_EQ, Literal: literal}
+		} else {
+			tok = newToken(token.BANG, l.ch)
+		}
+	case '*':
+		tok = newToken(token.ASTERISK, l.ch)
+	case '/':
+		tok = newToken(token.SLASH, l.ch)
+	case '<':
+		tok = newToken(token.LT, l.ch)
+	case '>':
+		tok = newToken(token.GT, l.ch)
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
@@ -108,14 +151,21 @@ func (l *Lexer) NextToken() token.Token {
 	return tok
 }
 
+// newToken take a tokenType and byte(ch) and returns a token with the tokenType as
+// the Type and a string of ch as the Literal. Note: newToken does not accept strings
+// as its second argument
 func newToken(tokenType token.TokenType, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
 }
 
+// isLetter returns the boolean true if the byte ch is a letter or an "_" and
+// false if not.
 func isLetter(ch byte) bool {
 	return 'a' <= ch && ch <= 'z' || 'A' <= ch && 'Z' <= ch || ch == '_'
 }
 
+// isDigit returns the boolean true if the byte ch is a digit or period and
+// false if not
 func isDigit(ch byte) bool {
 	return '0' <= ch && ch <= '9' || ch == '.'
 }
